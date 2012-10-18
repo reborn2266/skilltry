ca84f277930c557fbef03a7274e15684930f70d0
mars <reborn2266@gmail.com>
2012-10-19 00:01:28 +0800

    add descriptor

--- python/descriptor.py
+++ python/descriptor.py
@@ -1,13 +1,41 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-class foo:
-	def __init__(self, x, y):
-		self.__x = x
-		self.__y = y
-	@property
-	def x(self):
-		return self.__x
+class AClass(object):
+	def __init__(self):
+		self.d = 1
 
-if __name__ == "__main__":
-	afoo = foo(1, 3)
-	print(afoo.x)
+	def method(self, arg):
+		print arg
+
+aObj = AClass()
+
+# normal usage
+print aObj.d
+aObj.method("hello")
+
+# whole story of instance variable
+print aObj.__dict__['d']
+# why we need self as first parameter in method
+type(aObj).method(aObj, "hello")
+
+# how does the python find method
+type(aObj).__dict__['method'](aObj, "hello")
+
+# whole story of how the method be implemented by descriptor mechanism
+type(aObj).__dict__['method'].__get__(aObj, type(aObj))("hello")
+
+class Proxy(object):
+	def __get__(self, instance, cls):
+		def handle():
+			# access caller's info without passing any parameter
+			return str(cls.__name__) + " " + str(instance.name) + " happpy~"
+		return handle;
+
+class Host(object):
+	proxy = Proxy()
+	def __init__(self, name):
+		self.name = name
+
+host = Host("ERS")
+print host.proxy()
+print Host.__dict__['proxy'].__get__(host, Host)()
